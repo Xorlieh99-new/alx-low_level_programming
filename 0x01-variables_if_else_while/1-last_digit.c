f73a5c9979c8f39a237770b0fb8a42140b388143
Xorlieh99-new <67369756+Xorlieh99-new@users.noreply.github.com>
2022-09-12 12:44:45 +0200

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,19 +18,13 @@ int main(void)
 0-positive_or_negative.c {
 0-positive_or_negative.c printf("Last digit of %d is %d and is greater than 5\n", n, s);
 0-positive_or_negative.c }
-<<<<<<< HEAD
 0-positive_or_negative.c if (num == 0)
-=======
 0-positive_or_negative.c else if (s == 0)
->>>>>>> c38d008b18676f16774155151031a0fcc1c46752
 0-positive_or_negative.c {
 0-positive_or_negative.c printf("Last digit of %d is %d and is 0\n", n, s);
 0-positive_or_negative.c }
-<<<<<<< HEAD
 0-positive_or_negative.c if (num < 6)
-=======
 0-positive_or_negative.c else if (s < 6)
->>>>>>> c38d008b18676f16774155151031a0fcc1c46752
 0-positive_or_negative.c {
 0-positive_or_negative.c printf("Last digit of %d is %d and is less than 6 and not 0\n", n, s);
 0-positive_or_negative.c }
