fe547e3b401b54c2a4e555c742791d0edb9e7bcf
Xorlieh99-new <67369756+Xorlieh99-new@users.noreply.github.com>
2022-09-12 12:44:09 +0200

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,13 +6,12 @@
  */
 int main(void)
 {
-<<<<<<< HEAD
 int num;
 num = 97;
 while (num <= 122)
 putchar(num);
 num++;
-=======
+
 int p;
 p = 97;
 while (p <= 122)
@@ -20,7 +19,7 @@ while (p <= 122)
 putchar(p);
 p++;
 }
->>>>>>> c38d008b18676f16774155151031a0fcc1c46752
+
 putchar('\n');
 return (0);
 }
