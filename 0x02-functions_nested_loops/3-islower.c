d2f6676e5e73de64644c188771a28377e8cc0ae5
Xorlieh99-new <xorliehswah@gmail.com>
2022-09-19 02:00:15 -0700

    third file

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*_islower - checks for a lowercase character
+*@c: letter being tested
+* Return Always 0
+cat >> 3-islower.c << EOL
+#include "main.h"
+
+/**
+*_islower - checks for a lowercase character
+*@c: letter being tested
+* Return: Always 0
+*/
+
+int -slower(int c)
+{
+if(x >= 97 && c <= 122)
+return (1);
+else
+return (0);
+}
