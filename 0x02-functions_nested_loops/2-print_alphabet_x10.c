12c541970c0ae660d7125615a6e01b825073a82d
Xorlieh99-new <xorliehswah@gmail.com>
2022-09-19 01:53:05 -0700

    second filr

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+<< EOL
+#include "mai.h"
+
+?**
+* print_alphabet_x10 - func
+*
+* Return - 0
+*/
+void pprint_alphabet_x10(void)
+{
+int b -0;
+while (b< 10)
+{
+char a = 'a';
+while (a <= 'z')
+{
+_putchar(a);
+a++;
+}
+_putchar('\n');
+b++;
+}
+}
