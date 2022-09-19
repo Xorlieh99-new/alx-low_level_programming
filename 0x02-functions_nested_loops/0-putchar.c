a40c89871008eb5b290adfe2e233519e7b150e46
Xorlieh99-new <xorliehswah@gmail.com>
2022-09-19 01:14:26 -0700

    first file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,39 @@
+#include "putchar.h"
+/**
+* main - Prints Putchar
+*
+* Return: Always ) (Success)
+*/
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n')'
+cat >> 0-putchar.c << EOL
+#include "putchar.h"
+
+/**
+* main - Prints Putchar
+*
+* Return: Always ) (Success)
+*/
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+return(0);
+}
